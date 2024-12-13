16f4021afefb44253af2a55d6e23ec126cb57b8d
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2024-12-13 21:24:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
 module.exports = {
     creator: 'CAF-ID',
-    serviceAccount: { note: 'ini json' },
-    storageBucket: '',
+    serviceAccount: {
+  "type": "service_account",
+  "project_id": "agustyl",
+  "private_key_id": "e6e5c564af731e548172813adc08acd10016e488",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC11T1O2/v7U+D9\n11DWJ2ao5SnE6CdbDOQ1vSmANjPQtPSMxtL+AtC4C8y1pC/lqwgMZoHbm4bO9CCv\n9vMQfUI1O3FVrvIXPSGus8yl7hSgTbmbv3VwF9/Acve2CIIYSQYdOUUDIKJjONHX\n2FR1z7mjD52icQ5ZaMwYS5r8vHi2O4xZEPhtJd7vJJgL3cmWLLffAzc23QIneEBk\nisgRMiiJHxk2BKO372q2InhvJf1Q4lqgbYcG5tvMao3y5R26b1ylr2HgTkbv2tA2\nGMTeGBMapBbalB49KVGQb9FZ5/XRZUbw4XE+S4rd+d0oGW96vODHjc27t884nUGW\n8nUo4GOvAgMBAAECggEADjPkwQcfzIbFuS4bS07yjtivhYjYoh0EqknnNvOE4pVv\n0JsrGdogcW/CgqGbeCX+z/ElzYB9bCbXcscoPy55gGzvMlEi6bvFqL60inVpOQg5\nKjgvNR04RfD09kv91WXGy3uQr2jaJkPm6CQmnLkn99udZXXNOCCeaHtnVLxn9VvZ\ndubtrQOEHTJ9b2g5c/mhk/qDCdmLiaeB0x4oOOgVIYawN31bVXCJxkHpgIbd6elW\npPfV8IL4ZugWOWZK6/isAPWm07OAhxxrKe0UaT7+5bqe5hg9tykUDbFF3wj/rEis\nlyjOjkRCcybZksvxTLtkxzvmMPecuL9ESinjYYerkQKBgQDBSFuGEX3ycf+UwrDc\n6CESaHOsdziG6vHEHu/zP56aD/OABx5Xni53BVwS0MN1593fG6M6bsHn1tCRln8u\nlwDPaRPRx+XcNXzOA5wGYI5wWhdixBzfA8oL/KQJhxoHfX5xtB1gr6FNPg7eCab0\nBOAs4cdf/bwjZ4kyzhJoEB5HlwKBgQDw1ccSPf2ViURJjgGF5lovHAndclkltbJe\nvCD9ksyECnUs9AJpXdLLcWDW0aBedpuIwYcw0k9qY3+fxL4ioqCDQNNiazUlO9he\nZeKY3eLYk5Cfcq1EWuOWQSne6Z2JPm4dnB1Yqu4EpJqYN20gW99T5b3BdxQ908eK\nfMBeJEIHqQKBgGCjzR87n72ZQjEfJ09MwTxy/WM+OQU8c6Wb0l3NcX+WeimJZaJp\ndNIN2/uMZi6NwGHNb8/HkSxaO+6PUQf6eG08HKzHRibUl5JqO5j0rkN+yDKZ29Wm\nD1/u8hXRdzJHjPFNHWM6bWwxmz1plhBsX6oROZ7FP0x0eCTu5EUjygABAoGAJZ9f\nz+u7N6cP8eWrOz23DPD2YY/BeXCfAsmk5bgPFvmDQJiF3+v2SC3YEPcCdH9W4NBK\nzEeufFI617ymb0a0iIyR+SgCT2S/8k88TOEs1OSIjUAe7TS9pPVdEyOvpocIMLv4\noadiM3t7YxgVhh6UDskvEZAGq3nwDCRc8Nf9MLECgYEAu/RR5Tz0/GcffFXPu3t6\nqZPl2aPa9kMT0grfitnj8gsO2QpuQKKGNA0tXK0n6+Z3NHtFWkbHM5pR8MlK7ORm\n/Ltcblvz/Dl5lZdx0W5xXZ8ltrZaNZGiAVzc7kJq1XSxXgnEz3yAAQoAo0xBNtxq\n1q3ljBXNMEVe1slkgDGB8XY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-4x78l@agustyl.iam.gserviceaccount.com",
+  "client_id": "105663712536566044975",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4x78l%40agustyl.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+    },
+   storageBucket: '',
 }
